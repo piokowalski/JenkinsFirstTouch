b745cabb19d04f62713a948d02b83c3003daae0c
piokowalski <piokowalski@onet.pl>
2019-05-10 12:51:19 +0200

    First bad code review

--- src/main/java/pl/stormit/Service1.java
+++ src/main/java/pl/stormit/Service1.java
@@ -1,5 +1,23 @@
 package pl.stormit;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Service1 {
+    public static void main(String[] args) {
+
+
+        DateFormat dateFormat = new SimpleDateFormat();
+
+        Date date = new Date(2019, 1, 10);
+
+        String formattedDate = dateFormat.format(date);
+
+        System.out.println(formattedDate);
+    }
+
 
 }
+
+
