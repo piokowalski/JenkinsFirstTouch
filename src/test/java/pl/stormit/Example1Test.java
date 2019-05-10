9ce09a28e2d52a6eb8cf4011468fd9bd03ce3089
piokowalski <piokowalski@onet.pl>
2019-05-10 12:37:40 +0200

    Still 1 test failed.

--- src/test/java/pl/stormit/Example1Test.java
+++ src/test/java/pl/stormit/Example1Test.java
@@ -7,22 +7,18 @@ public class Example1Test {
 
 	@Test
 	public void test1() {
-
 	}
 
 	@Test
 	public void test2() {
-
 	}
 
 	@Test
 	public void test3() {
-
 	}
 
 	@Test
 	public void test4() {
-
 	}
 
 	@Test
