7bd4be7600972e1f717ef1c5f83b69f6fa557e99
piokowalski <piokowalski@onet.pl>
2019-05-10 12:54:09 +0200

    Second bad code reviewed

--- src/main/java/pl/stormit/Service1.java
+++ src/main/java/pl/stormit/Service1.java
@@ -8,6 +8,7 @@ public class Service1 {
     public static void main(String[] args) {
 
 
+        //Example1
         DateFormat dateFormat = new SimpleDateFormat();
 
         Date date = new Date(2019, 1, 10);
@@ -15,6 +16,13 @@ public class Service1 {
         String formattedDate = dateFormat.format(date);
 
         System.out.println(formattedDate);
+
+        //Example2
+        int a = 2_147_483_646;
+        int b = 1;
+        int result = a + b;
+
+        System.out.println(result);
     }
 
 
